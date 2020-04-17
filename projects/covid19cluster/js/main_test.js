edc7724ee949a2a95314698536a5f4e0c338effb
louislugas <louislugas@gmail.com>
2020-04-18 00:47:30 +0700

    covid json

--- projects/covid19cluster/js/main_test.js
+++ projects/covid19cluster/js/main_test.js
@@ -35,7 +35,7 @@ zoom_handler(svg);
 var radioValue;
 
 //data url
-var url = "/covid_19_cluster/json/kasus-corona-indonesia.json";
+var url = "/covid19cluster/json/kasus-corona-indonesia.json";
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 //UPDATE FORCE NORMAL
